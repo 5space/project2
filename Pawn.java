e4cd406bbee4f84952f63774836fba705974be07
muzak23 <logan.nommensen@gmail.com>
2022-02-08 16:05:00 -0500

    Adds pawn movement

--- Pawn.java
+++ Pawn.java
@@ -1,6 +1,7 @@
 package project2;
 
 public class Pawn extends ChessPiece {
+	boolean hasMoved;
 
 	public Pawn(Player player) {
 		super(player);
@@ -10,11 +11,40 @@ public class Pawn extends ChessPiece {
 		return "Pawn";
 	}
 
+	@Override
 	// determines if the move is valid for a pawn piece
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
-
-		boolean valid = true;
-        // More code is needed
-		return valid;
+		if (player() == Player.WHITE) {
+			// Normal 1 move forward
+			if (move.fromColumn == move.toColumn && move.fromRow == move.toRow + 1 && board[move.toRow][move.toColumn] == null) {
+				return true;
+			}
+			// If the pawn hasn't moved yet, it can move 2 spaces forward
+			if (!hasMoved && move.fromColumn == move.toColumn && move.fromRow == move.toRow + 2 && board[move.toRow][move.toColumn] == null) {
+				hasMoved = true;
+				return true;
+			}
+			// Can move diagonally forward if there is an enemy piece there
+			if ((move.fromColumn == move.toColumn + 1 || move.fromColumn == move.toColumn - 1) &&
+					(move.fromRow == move.toRow + 1) && board[move.toRow][move.toColumn].player() == Player.BLACK) {
+				return true;
+			}
+		} else {
+			// Normal 1 move forward
+			if (move.fromColumn == move.toColumn && move.fromRow == move.toRow - 1 && board[move.toRow][move.toColumn] == null) {
+				return true;
+			}
+			// If the pawn hasn't moved yet, it can move 2 spaces forward
+			if (!hasMoved && move.fromColumn == move.toColumn && move.fromRow == move.toRow - 2 && board[move.toRow][move.toColumn] == null) {
+				hasMoved = true;
+				return true;
+			}
+			// Can move diagonally forward if there is an enemy piece there
+			if ((move.fromColumn == move.toColumn + 1 || move.fromColumn == move.toColumn - 1) &&
+					(move.fromRow == move.toRow - 1) && board[move.toRow][move.toColumn].player() == Player.WHITE) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
