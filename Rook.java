3cd1aa084a222d6beb7e2875a3549a1a98f57120
muzak23 <logan.nommensen@gmail.com>
2022-02-11 14:46:58 -0500

    Finished basic rook movement (excl. castling)

--- Rook.java
+++ Rook.java
@@ -3,24 +3,72 @@ package project2;
 public class Rook extends ChessPiece {
 
 	public Rook(Player player) {
-		
 		super(player);
-		
 	}
 
 	public String type() {
-		
 		return "Rook";
-		
 	}
-	
+
 	// determines if the move is valid for a rook piece
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		
-		boolean valid = true;
-        // More code is needed
-        return valid;
-		
+		// If the move is the same spot, it is not valid
+		if (move.fromColumn == move.toColumn && move.fromRow == move.toRow) {
+			return false;
+		}
+
+		// If the move is in a different column AND row, it is not valid
+		if (move.fromColumn != move.toColumn && move.fromRow != move.toRow) {
+			return false;
+		}
+
+		// Moving vertically
+		if (move.fromColumn == move.toColumn) {
+			// Moving down
+			if (move.fromRow < move.toRow) {
+				// Checks all spots between from and to (exclusive)
+				for (int i = move.fromRow + 1; i < move.toRow; i++) {
+					// If spot is occupied
+					if (board[i][move.fromColumn] != null) {
+						return false;
+					}
+				}
+			}
+			// Moving up
+			else {
+				// Checks all spots between from and to (exclusive)
+				for (int i = move.fromRow - 1; i > move.toRow; i--) {
+					// If spot is occupied
+					if (board[i][move.fromColumn] != null) {
+						return false;
+					}
+				}
+			}
+		}
+		// Moving horizontally
+		else {
+			// Moving right
+			if (move.fromColumn < move.toColumn) {
+				// Checks all spots between from and to (exclusive)
+				for (int i = move.fromColumn + 1; i < move.toColumn; i++) {
+					// If spot is occupied
+					if (board[move.fromRow][i] != null) {
+						return false;
+					}
+				}
+			}
+			// Moving left
+			else {
+				// Checks all spots between from and to (exclusive)
+				for (int i = move.fromColumn - 1; i > move.toColumn; i--) {
+					// If spot is occupied
+					if (board[move.fromRow][i] != null) {
+						return false;
+					}
+				}
+			}
+		}
+		// If there's no piece in the way, and the destination isn't the same player's
+		return board[move.toRow][move.toColumn] == null || board[move.toRow][move.toColumn].player() != player();
 	}
-	
 }
