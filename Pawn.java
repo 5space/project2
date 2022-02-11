34215036e46fc065fd2946d42c7efce2b2a45b5a
muzak23 <logan.nommensen@gmail.com>
2022-02-11 17:34:40 -0500

    Fixed missing hasMoved and implemented the ChessPiece isValidMove

--- Pawn.java
+++ Pawn.java
@@ -14,9 +14,15 @@ public class Pawn extends ChessPiece {
 	@Override
 	// determines if the move is valid for a pawn piece
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		if (!super.isValidMove(move, board)) {
+			return false;
+		}
+
+
 		if (player() == Player.WHITE) {
 			// Normal 1 move forward
 			if (move.fromColumn == move.toColumn && move.fromRow == move.toRow + 1 && board[move.toRow][move.toColumn] == null) {
+				hasMoved = true;
 				return true;
 			}
 			// If the pawn hasn't moved yet, it can move 2 spaces forward
@@ -27,11 +33,13 @@ public class Pawn extends ChessPiece {
 			// Can move diagonally forward if there is an enemy piece there
 			if ((move.fromColumn == move.toColumn + 1 || move.fromColumn == move.toColumn - 1) &&
 					(move.fromRow == move.toRow + 1) && board[move.toRow][move.toColumn].player() == Player.BLACK) {
+				hasMoved = true;
 				return true;
 			}
 		} else {
 			// Normal 1 move forward
 			if (move.fromColumn == move.toColumn && move.fromRow == move.toRow - 1 && board[move.toRow][move.toColumn] == null) {
+				hasMoved = true;
 				return true;
 			}
 			// If the pawn hasn't moved yet, it can move 2 spaces forward
@@ -42,6 +50,7 @@ public class Pawn extends ChessPiece {
 			// Can move diagonally forward if there is an enemy piece there
 			if ((move.fromColumn == move.toColumn + 1 || move.fromColumn == move.toColumn - 1) &&
 					(move.fromRow == move.toRow - 1) && board[move.toRow][move.toColumn].player() == Player.WHITE) {
+				hasMoved = true;
 				return true;
 			}
 		}
