b67f3b08a1642c34cb05021d41624f28fe59cfac
muzak23 <logan.nommensen@gmail.com>
2022-02-15 16:13:59 -0500

    Implemented Bishop movement

--- Bishop.java
+++ Bishop.java
@@ -11,9 +11,50 @@ public class Bishop extends ChessPiece {
 	}
 	
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		if (!super.isValidMove(move, board)) {
+			return false;
+		}
 
+		// Checks that the move is diagonal
+		if (Math.abs(move.toRow - move.fromRow) != Math.abs(move.toColumn - move.fromColumn)) {
+			return false;
+		}
+
+		// Diagonal check up left
+		if (move.toRow < move.fromRow && move.toColumn < move.fromColumn) {
+			for (int i = 1; i < (move.fromRow - move.toRow); i++) {
+				if (board[move.fromRow - i][move.fromColumn - i] != null) {
+					return false;
+				}
+			}
+		}
+		// Diagonal check up right
+		else if (move.toRow < move.fromRow && move.toColumn > move.fromColumn) {
+			for (int i = 1; i < (move.fromRow - move.toRow); i++) {
+				if (board[move.fromRow - i][move.fromColumn + i] != null) {
+					return false;
+				}
+			}
+		}
+		// Diagonal check down left
+		else if (move.toRow > move.fromRow && move.toColumn < move.fromColumn) {
+			for (int i = 1; i < (move.toRow - move.fromRow); i++) {
+				if (board[move.fromRow + i][move.fromColumn - i] != null) {
+					return false;
+				}
+			}
+		}
+		// Diagonal check down right
+		else if (move.toRow > move.fromRow && move.toColumn > move.fromColumn) {
+			for (int i = 1; i < (move.toRow - move.fromRow); i++) {
+				if (board[move.fromRow + i][move.fromColumn + i] != null) {
+					return false;
+				}
+			}
+		}
+
+		// If there's no piece in the way
 		return true;
-        // More code is needed
-		
+
 	}
 }
